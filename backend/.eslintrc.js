c55811df194cc3448f081f71bb6802190f0a2c62
Romain Vitry <romain.vitry@gmail.com>
2026-01-26 17:15:17 +0100

    fix(ci): Add ESLint config for backend

---
+++ backend/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  env: {
+    node: true,
+    es2022: true,
+    jest: true,
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'no-console': 'off',
+  },
+  ignorePatterns: ['dist/', 'node_modules/', 'coverage/', '*.js'],
+};
